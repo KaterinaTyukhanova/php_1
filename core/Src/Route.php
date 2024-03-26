00ec923e64e364bc13a91f1c3960491912033df4
KaterinaTyukhanova <klimovafiona@gmail.com>
2024-03-26 23:45:02 +0700

    Задание 2: Работа с сессиями

--- core/Src/Route.php
+++ core/Src/Route.php
@@ -9,6 +9,11 @@ class Route
     private static array $routes = [];
     private static string $prefix = '';
 
+    public function __construct(string $prefix = '')
+    {
+        self::setPrefix($prefix);
+    }
+
     public static function setPrefix($value)
     {
         self::$prefix = $value;
@@ -43,5 +48,15 @@ class Route
 
 
         call_user_func([new $class, $action], new Request());
+    }
+
+    public function redirect(string $url): void
+    {
+        header('Location: ' . $this->getUrl($url));
+    }
+
+    public function getUrl(string $url): string
+    {
+        return self::$prefix . $url;
     }
 }
